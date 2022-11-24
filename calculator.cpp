1e1e7adc8783f3aca3d4ab9032156c0f9dcd4b2b
Rajiv-kumarr <109952943+Rajiv-kumarr@users.noreply.github.com>
2022-11-24 09:08:15 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cassert>
+#include <limits>
+using namespace std;
+
+int addition(int num1, int num2)
+{
+    cout<<num1<<"+"<<num2<<"="<<num1+num2<<endl<<endl;
+    return num1+num2;
+}
+
+unsigned int addition(unsigned int num1, unsigned int num2)
+{
+    cout<<num1<<"+"<<num2<<"="<<num1+num2<<endl<<endl;
+    return num1+num2;
+}
+
+int subtraction(int num1, int num2)
+{
+    cout<<num1<<"-"<<num2<<"="<<num1-num2<<endl<<endl;
+    return num1-num2;
+}
+
+unsigned int subtraction(unsigned int num1, unsigned int num2)
+{
+    cout<<num1<<"-"<<num2<<"="<<num1-num2<<endl<<endl;
+    return num1-num2;
+}
+
+int multiplication(int num1, int num2)
+{
+    cout<<num1<<"*"<<num2<<"="<<num1*num2<<endl;
+    return num1*num2;
+}
+
+unsigned int multiplication(unsigned int num1, unsigned int num2)
+{
+    cout<<num1<<"*"<<num2<<"="<<num1*num2<<endl;
+    return num1-num2;
+}
+
+float division(float num1, float num2)
+{
+    float div=num1/num2;
+    cout<<num1<<"/"<<num2<<"="<<div<<endl;
+    return num1/num2;
+}
+
+int main()
+{   
+    int num1,num2;
+    cout<<"Enter the 2 numbers : ";
+    cin>>num1;
+    cin>>num2;
+    addition(num1, num2);
+    subtraction(num1, num2);
+    multiplication(num1, num2);
+    division(num1, num2);
+    return 0;
+}
