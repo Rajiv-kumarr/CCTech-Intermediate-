9990289a381e8cc0096caf2894139b8d626ff142
Rajiv-kumarr <109952943+Rajiv-kumarr@users.noreply.github.com>
2022-12-12 22:10:00 +0530

    Create sort_by_length.cpp

---
+++ modular_programming/sort_by_length.cpp
@@ -0,0 +1,12 @@
+#include "sort_string_by_length.h"
+#include "sort_string_by_length.cpp"
+
+int main(int argc, char* argv[])
+{
+    if(argc==1)
+    {
+        sort_string_by_length();
+    }
+
+    return 0;
+}
